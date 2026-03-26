eedc9dd54a27f6d82e5bc6504fb0dd766d945bcb
fruittytea <sadkovaanna46@gmail.com>
2026-03-26 09:43:45 +0300

    Update Task3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,393 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Button ButtonRemove;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            OutputWindow = new Label();
+            ButtonPlus = new Button();
+            ButtonMinus = new Button();
+            ButtonMultiply = new Button();
+            ButtonDivide = new Button();
+            ButtonClearAll = new Button();
+            ButtonOne = new Button();
+            ButtonTwo = new Button();
+            ButtonThree = new Button();
+            ButtonFour = new Button();
+            ButtonFive = new Button();
+            ButtonSix = new Button();
+            ButtonSeven = new Button();
+            ButtonEight = new Button();
+            ButtonNine = new Button();
+            ButtonNull = new Button();
+            ButtonLeftParenthesis = new Button();
+            ButtonRightParenthesis = new Button();
+            ButtonPoint = new Button();
+            ButtonPercent = new Button();
+            ButtonEquals = new Button();
+            ButtonCos = new Button();
+            ButtonSin = new Button();
+            ButtonTg = new Button();
+            ButtonLn = new Button();
+            ButtonExponentiation = new Button();
+            ButtonSqrt = new Button();
+            ButtonLog = new Button();
+            ButtonE = new Button();
+            ButtonPi = new Button();
+            ButtonRemove = new Button();
             SuspendLayout();
+            // 
+            // ButtonRemove
+            // 
+            resources.ApplyResources(ButtonRemove, "ButtonRemove");
+            ButtonRemove.BackColor = Color.Transparent;
+            ButtonRemove.BackgroundImage = Properties.Resources.remove;
+            ButtonRemove.Cursor = Cursors.Hand;
+            ButtonRemove.ForeColor = Color.Transparent;
+            ButtonRemove.Name = "ButtonRemove";
+            ButtonRemove.UseVisualStyleBackColor = false;
+            ButtonRemove.Click += ButtonRemove_Click;
+            // 
+            // OutputWindow
+            // 
+            resources.ApplyResources(OutputWindow, "OutputWindow");
+            OutputWindow.BorderStyle = BorderStyle.FixedSingle;
+            OutputWindow.ForeColor = Color.FromArgb(30, 30, 143);
+            OutputWindow.Name = "OutputWindow";
+            // 
+            // ButtonPlus
+            // 
+            resources.ApplyResources(ButtonPlus, "ButtonPlus");
+            ButtonPlus.Cursor = Cursors.Hand;
+            ButtonPlus.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonPlus.Name = "ButtonPlus";
+            ButtonPlus.UseVisualStyleBackColor = true;
+            ButtonPlus.Click += ButtonPlus_Click;
+            // 
+            // ButtonMinus
+            // 
+            resources.ApplyResources(ButtonMinus, "ButtonMinus");
+            ButtonMinus.Cursor = Cursors.Hand;
+            ButtonMinus.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonMinus.Name = "ButtonMinus";
+            ButtonMinus.UseVisualStyleBackColor = true;
+            ButtonMinus.Click += ButtonMinus_Click;
+            // 
+            // ButtonMultiply
+            // 
+            resources.ApplyResources(ButtonMultiply, "ButtonMultiply");
+            ButtonMultiply.Cursor = Cursors.Hand;
+            ButtonMultiply.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonMultiply.Name = "ButtonMultiply";
+            ButtonMultiply.UseVisualStyleBackColor = true;
+            ButtonMultiply.Click += ButtonMultiply_Click;
+            // 
+            // ButtonDivide
+            // 
+            resources.ApplyResources(ButtonDivide, "ButtonDivide");
+            ButtonDivide.Cursor = Cursors.Hand;
+            ButtonDivide.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonDivide.Name = "ButtonDivide";
+            ButtonDivide.UseVisualStyleBackColor = true;
+            ButtonDivide.Click += ButtonDivide_Click;
+            // 
+            // ButtonClearAll
+            // 
+            resources.ApplyResources(ButtonClearAll, "ButtonClearAll");
+            ButtonClearAll.Cursor = Cursors.Hand;
+            ButtonClearAll.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonClearAll.Name = "ButtonClearAll";
+            ButtonClearAll.UseVisualStyleBackColor = true;
+            ButtonClearAll.Click += ButtonClearAll_Click;
+            // 
+            // ButtonOne
+            // 
+            resources.ApplyResources(ButtonOne, "ButtonOne");
+            ButtonOne.Cursor = Cursors.Hand;
+            ButtonOne.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonOne.Name = "ButtonOne";
+            ButtonOne.UseVisualStyleBackColor = true;
+            ButtonOne.Click += ButtonOne_Click;
+            // 
+            // ButtonTwo
+            // 
+            resources.ApplyResources(ButtonTwo, "ButtonTwo");
+            ButtonTwo.Cursor = Cursors.Hand;
+            ButtonTwo.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonTwo.Name = "ButtonTwo";
+            ButtonTwo.UseVisualStyleBackColor = true;
+            ButtonTwo.Click += ButtonTwo_Click;
+            // 
+            // ButtonThree
+            // 
+            resources.ApplyResources(ButtonThree, "ButtonThree");
+            ButtonThree.Cursor = Cursors.Hand;
+            ButtonThree.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonThree.Name = "ButtonThree";
+            ButtonThree.UseVisualStyleBackColor = true;
+            ButtonThree.Click += ButtonThree_Click;
+            // 
+            // ButtonFour
+            // 
+            resources.ApplyResources(ButtonFour, "ButtonFour");
+            ButtonFour.Cursor = Cursors.Hand;
+            ButtonFour.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonFour.Name = "ButtonFour";
+            ButtonFour.UseVisualStyleBackColor = true;
+            ButtonFour.Click += ButtonFour_Click;
+            // 
+            // ButtonFive
+            // 
+            resources.ApplyResources(ButtonFive, "ButtonFive");
+            ButtonFive.Cursor = Cursors.Hand;
+            ButtonFive.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonFive.Name = "ButtonFive";
+            ButtonFive.UseVisualStyleBackColor = true;
+            ButtonFive.Click += ButtonFive_Click;
+            // 
+            // ButtonSix
+            // 
+            resources.ApplyResources(ButtonSix, "ButtonSix");
+            ButtonSix.Cursor = Cursors.Hand;
+            ButtonSix.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonSix.Name = "ButtonSix";
+            ButtonSix.UseVisualStyleBackColor = true;
+            ButtonSix.Click += ButtonSix_Click;
+            // 
+            // ButtonSeven
+            // 
+            resources.ApplyResources(ButtonSeven, "ButtonSeven");
+            ButtonSeven.Cursor = Cursors.Hand;
+            ButtonSeven.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonSeven.Name = "ButtonSeven";
+            ButtonSeven.UseVisualStyleBackColor = true;
+            ButtonSeven.Click += ButtonSeven_Click;
+            // 
+            // ButtonEight
+            // 
+            resources.ApplyResources(ButtonEight, "ButtonEight");
+            ButtonEight.Cursor = Cursors.Hand;
+            ButtonEight.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonEight.Name = "ButtonEight";
+            ButtonEight.UseVisualStyleBackColor = true;
+            ButtonEight.Click += ButtonEight_Click;
+            // 
+            // ButtonNine
+            // 
+            resources.ApplyResources(ButtonNine, "ButtonNine");
+            ButtonNine.Cursor = Cursors.Hand;
+            ButtonNine.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonNine.Name = "ButtonNine";
+            ButtonNine.UseVisualStyleBackColor = true;
+            ButtonNine.Click += ButtonNine_Click;
+            // 
+            // ButtonNull
+            // 
+            resources.ApplyResources(ButtonNull, "ButtonNull");
+            ButtonNull.Cursor = Cursors.Hand;
+            ButtonNull.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonNull.Name = "ButtonNull";
+            ButtonNull.UseVisualStyleBackColor = true;
+            ButtonNull.Click += ButtonNull_Click;
+            // 
+            // ButtonLeftParenthesis
+            // 
+            resources.ApplyResources(ButtonLeftParenthesis, "ButtonLeftParenthesis");
+            ButtonLeftParenthesis.Cursor = Cursors.Hand;
+            ButtonLeftParenthesis.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonLeftParenthesis.Name = "ButtonLeftParenthesis";
+            ButtonLeftParenthesis.UseVisualStyleBackColor = true;
+            ButtonLeftParenthesis.Click += ButtonLeftParenthesis_Click;
+            // 
+            // ButtonRightParenthesis
+            // 
+            resources.ApplyResources(ButtonRightParenthesis, "ButtonRightParenthesis");
+            ButtonRightParenthesis.Cursor = Cursors.Hand;
+            ButtonRightParenthesis.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonRightParenthesis.Name = "ButtonRightParenthesis";
+            ButtonRightParenthesis.UseVisualStyleBackColor = true;
+            ButtonRightParenthesis.Click += ButtonRightParenthesis_Click;
+            // 
+            // ButtonPoint
+            // 
+            resources.ApplyResources(ButtonPoint, "ButtonPoint");
+            ButtonPoint.Cursor = Cursors.Hand;
+            ButtonPoint.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonPoint.Name = "ButtonPoint";
+            ButtonPoint.UseVisualStyleBackColor = true;
+            ButtonPoint.Click += ButtonPoint_Click;
+            // 
+            // ButtonPercent
+            // 
+            resources.ApplyResources(ButtonPercent, "ButtonPercent");
+            ButtonPercent.Cursor = Cursors.Hand;
+            ButtonPercent.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonPercent.Name = "ButtonPercent";
+            ButtonPercent.UseVisualStyleBackColor = true;
+            ButtonPercent.Click += ButtonPercent_Click;
+            // 
+            // ButtonEquals
+            // 
+            resources.ApplyResources(ButtonEquals, "ButtonEquals");
+            ButtonEquals.Cursor = Cursors.Hand;
+            ButtonEquals.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonEquals.Name = "ButtonEquals";
+            ButtonEquals.UseVisualStyleBackColor = true;
+            // 
+            // ButtonCos
+            // 
+            resources.ApplyResources(ButtonCos, "ButtonCos");
+            ButtonCos.Cursor = Cursors.Hand;
+            ButtonCos.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonCos.Name = "ButtonCos";
+            ButtonCos.UseVisualStyleBackColor = true;
+            ButtonCos.Click += ButtonCos_Click;
+            // 
+            // ButtonSin
+            // 
+            resources.ApplyResources(ButtonSin, "ButtonSin");
+            ButtonSin.Cursor = Cursors.Hand;
+            ButtonSin.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonSin.Name = "ButtonSin";
+            ButtonSin.UseVisualStyleBackColor = true;
+            ButtonSin.Click += ButtonSin_Click;
+            // 
+            // ButtonTg
+            // 
+            resources.ApplyResources(ButtonTg, "ButtonTg");
+            ButtonTg.Cursor = Cursors.Hand;
+            ButtonTg.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonTg.Name = "ButtonTg";
+            ButtonTg.UseVisualStyleBackColor = true;
+            ButtonTg.Click += ButtonTg_Click;
+            // 
+            // ButtonLn
+            // 
+            resources.ApplyResources(ButtonLn, "ButtonLn");
+            ButtonLn.Cursor = Cursors.Hand;
+            ButtonLn.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonLn.Name = "ButtonLn";
+            ButtonLn.UseVisualStyleBackColor = true;
+            ButtonLn.Click += ButtonLn_Click;
+            // 
+            // ButtonExponentiation
+            // 
+            resources.ApplyResources(ButtonExponentiation, "ButtonExponentiation");
+            ButtonExponentiation.Cursor = Cursors.Hand;
+            ButtonExponentiation.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonExponentiation.Name = "ButtonExponentiation";
+            ButtonExponentiation.UseVisualStyleBackColor = true;
+            ButtonExponentiation.Click += ButtonExponentiation_Click;
+            // 
+            // ButtonSqrt
+            // 
+            resources.ApplyResources(ButtonSqrt, "ButtonSqrt");
+            ButtonSqrt.Cursor = Cursors.Hand;
+            ButtonSqrt.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonSqrt.Name = "ButtonSqrt";
+            ButtonSqrt.UseVisualStyleBackColor = true;
+            ButtonSqrt.Click += ButtonSqrt_Click;
+            // 
+            // ButtonLog
+            // 
+            resources.ApplyResources(ButtonLog, "ButtonLog");
+            ButtonLog.Cursor = Cursors.Hand;
+            ButtonLog.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonLog.Name = "ButtonLog";
+            ButtonLog.UseVisualStyleBackColor = true;
+            ButtonLog.Click += ButtonLog_Click;
+            // 
+            // ButtonE
+            // 
+            resources.ApplyResources(ButtonE, "ButtonE");
+            ButtonE.Cursor = Cursors.Hand;
+            ButtonE.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonE.Name = "ButtonE";
+            ButtonE.UseVisualStyleBackColor = true;
+            ButtonE.Click += ButtonE_Click;
+            // 
+            // ButtonPi
+            // 
+            resources.ApplyResources(ButtonPi, "ButtonPi");
+            ButtonPi.Cursor = Cursors.Hand;
+            ButtonPi.ForeColor = Color.FromArgb(30, 30, 143);
+            ButtonPi.Name = "ButtonPi";
+            ButtonPi.UseVisualStyleBackColor = true;
+            ButtonPi.Click += ButtonPi_Click;
             // 
             // Calculator
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Font = new Font("Malgun Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Controls.Add(ButtonPi);
+            Controls.Add(ButtonE);
+            Controls.Add(ButtonLog);
+            Controls.Add(ButtonSqrt);
+            Controls.Add(ButtonExponentiation);
+            Controls.Add(ButtonLn);
+            Controls.Add(ButtonTg);
+            Controls.Add(ButtonSin);
+            Controls.Add(ButtonCos);
+            Controls.Add(ButtonEquals);
+            Controls.Add(ButtonPercent);
+            Controls.Add(ButtonPoint);
+            Controls.Add(ButtonRightParenthesis);
+            Controls.Add(ButtonLeftParenthesis);
+            Controls.Add(ButtonNull);
+            Controls.Add(ButtonNine);
+            Controls.Add(ButtonEight);
+            Controls.Add(ButtonSeven);
+            Controls.Add(ButtonSix);
+            Controls.Add(ButtonFive);
+            Controls.Add(ButtonFour);
+            Controls.Add(ButtonThree);
+            Controls.Add(ButtonTwo);
+            Controls.Add(ButtonOne);
+            Controls.Add(ButtonRemove);
+            Controls.Add(ButtonClearAll);
+            Controls.Add(ButtonDivide);
+            Controls.Add(ButtonMultiply);
+            Controls.Add(ButtonMinus);
+            Controls.Add(ButtonPlus);
+            Controls.Add(OutputWindow);
+            ForeColor = Color.FromArgb(30, 30, 143);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
             Name = "Calculator";
-            Text = "Калькулятор";
             Load += Calculator_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label OutputWindow;
+        private Button ButtonPlus;
+        private Button ButtonMinus;
+        private Button ButtonMultiply;
+        private Button ButtonDivide;
+        private Button ButtonClearAll;
+        private Button ButtonRemove;
+        private Button ButtonOne;
+        private Button ButtonTwo;
+        private Button ButtonThree;
+        private Button ButtonFour;
+        private Button ButtonFive;
+        private Button ButtonSix;
+        private Button ButtonSeven;
+        private Button ButtonEight;
+        private Button ButtonNine;
+        private Button ButtonNull;
+        private Button ButtonLeftParenthesis;
+        private Button ButtonRightParenthesis;
+        private Button ButtonPoint;
+        private Button ButtonPercent;
+        private Button ButtonEquals;
+        private Button ButtonCos;
+        private Button ButtonSin;
+        private Button ButtonTg;
+        private Button ButtonLn;
+        private Button ButtonExponentiation;
+        private Button ButtonSqrt;
+        private Button ButtonLog;
+        private Button ButtonE;
+        private Button ButtonPi;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,5 +11,155 @@ namespace Task3_Calculator
         {
 
         }
+
+        private void ButtonDivide_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "/";
+        }
+
+        private void ButtonLeftParenthesis_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "(";
+        }
+
+        private void ButtonRightParenthesis_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += ")";
+        }
+
+        private void ButtonPercent_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "%";
+        }
+
+        private void ButtonPlus_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "+";
+        }
+
+        private void ButtonOne_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "1";
+        }
+
+        private void ButtonTwo_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "2";
+        }
+
+        private void ButtonThree_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "3";
+        }
+
+        private void ButtonFour_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "4";
+        }
+
+        private void ButtonFive_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "5";
+        }
+
+        private void ButtonSix_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "6";
+        }
+
+        private void ButtonSeven_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "7";
+        }
+
+        private void ButtonEight_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "8";
+        }
+
+        private void ButtonNine_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "9";
+        }
+
+        private void ButtonNull_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "0";
+        }
+
+        private void ButtonPoint_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += ".";
+        }
+
+        private void ButtonMinus_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "-";
+        }
+
+        private void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "x";
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            string OutputText = OutputWindow.Text;
+            if (OutputText.Length > 0)
+            {
+                OutputText = OutputText.Remove(OutputText.Length - 1);
+            }
+            OutputWindow.Text = OutputText;
+        }
+
+        private void ButtonClearAll_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text = "";
+        }
+
+        private void ButtonCos_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "cos(";
+        }
+
+        private void ButtonSin_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "sin(";
+        }
+
+        private void ButtonTg_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "tg(";
+        }
+
+        private void ButtonExponentiation_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "^";
+        }
+
+        private void ButtonSqrt_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "sqrt(";
+        }
+
+        private void ButtonE_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "e";
+        }
+
+        private void ButtonLn_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "ln(";
+        }
+
+        private void ButtonLog_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "log(";
+        }
+
+        private void ButtonPi_Click(object sender, EventArgs e)
+        {
+            OutputWindow.Text += "π";
+        }
     }
 }
