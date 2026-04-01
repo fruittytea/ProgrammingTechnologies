f1525b4fcf47e65ea9018f483fc63219e46a8e5f
fruittytea <sadkovaanna46@gmail.com>
2026-04-01 14:58:06 +0300

    Добавление функции открытия файла

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            openFileDialog1 = new OpenFileDialog();
-            saveFileDialog1 = new SaveFileDialog();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextEditor));
+            OpenFileDialog = new OpenFileDialog();
+            SaveFileDialog = new SaveFileDialog();
             ActionsMenu = new MenuStrip();
             открытьToolStripMenuItem = new ToolStripMenuItem();
             OpenFileButton = new ToolStripMenuItem();
@@ -37,21 +38,24 @@
             SaveFileButton = new ToolStripMenuItem();
             SaveAsButton = new ToolStripMenuItem();
             AppearanceButton = new ToolStripMenuItem();
-            fontDialog1 = new FontDialog();
-            colorDialog1 = new ColorDialog();
+            FontDialog = new FontDialog();
+            ColorDialog = new ColorDialog();
+            TextArea = new RichTextBox();
             ActionsMenu.SuspendLayout();
             SuspendLayout();
             // 
-            // openFileDialog1
+            // OpenFileDialog
             // 
-            openFileDialog1.FileName = "openFileDialog1";
+            OpenFileDialog.FileName = "openFileDialog1";
             // 
             // ActionsMenu
             // 
+            ActionsMenu.BackColor = Color.Silver;
+            ActionsMenu.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
             ActionsMenu.Items.AddRange(new ToolStripItem[] { открытьToolStripMenuItem, AppearanceButton });
             ActionsMenu.Location = new Point(0, 0);
             ActionsMenu.Name = "ActionsMenu";
-            ActionsMenu.Size = new Size(800, 24);
+            ActionsMenu.Size = new Size(984, 34);
             ActionsMenu.TabIndex = 1;
             ActionsMenu.Text = "menuStrip1";
             // 
@@ -59,46 +63,64 @@
             // 
             открытьToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { OpenFileButton, SaveButton });
             открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            открытьToolStripMenuItem.Size = new Size(48, 20);
+            открытьToolStripMenuItem.Padding = new Padding(10, 3, 10, 3);
+            открытьToolStripMenuItem.Size = new Size(69, 30);
             открытьToolStripMenuItem.Text = "Файл";
             // 
             // OpenFileButton
             // 
             OpenFileButton.Name = "OpenFileButton";
-            OpenFileButton.Size = new Size(142, 22);
+            OpenFileButton.Size = new Size(180, 24);
             OpenFileButton.Text = "Открыть";
+            OpenFileButton.Click += OpenFileButton_Click;
             // 
             // SaveButton
             // 
             SaveButton.DropDownItems.AddRange(new ToolStripItem[] { SaveFileButton, SaveAsButton });
             SaveButton.Name = "SaveButton";
-            SaveButton.Size = new Size(142, 22);
+            SaveButton.Size = new Size(180, 24);
             SaveButton.Text = "Сохранить...";
             // 
             // SaveFileButton
             // 
             SaveFileButton.Name = "SaveFileButton";
-            SaveFileButton.Size = new Size(154, 22);
+            SaveFileButton.Size = new Size(178, 24);
             SaveFileButton.Text = "Сохранить";
             // 
             // SaveAsButton
             // 
             SaveAsButton.Name = "SaveAsButton";
-            SaveAsButton.Size = new Size(154, 22);
+            SaveAsButton.Size = new Size(178, 24);
             SaveAsButton.Text = "Сохранить как";
             // 
             // AppearanceButton
             // 
             AppearanceButton.Name = "AppearanceButton";
-            AppearanceButton.Size = new Size(39, 20);
+            AppearanceButton.Padding = new Padding(10, 3, 10, 3);
+            AppearanceButton.Size = new Size(59, 30);
             AppearanceButton.Text = "Вид";
+            // 
+            // TextArea
+            // 
+            TextArea.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            TextArea.BorderStyle = BorderStyle.None;
+            TextArea.Cursor = Cursors.IBeam;
+            TextArea.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            TextArea.Location = new Point(0, 37);
+            TextArea.Margin = new Padding(20, 100, 20, 100);
+            TextArea.Name = "TextArea";
+            TextArea.Size = new Size(984, 524);
+            TextArea.TabIndex = 1;
+            TextArea.Text = "";
             // 
             // TextEditor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(984, 561);
+            Controls.Add(TextArea);
             Controls.Add(ActionsMenu);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = ActionsMenu;
             Name = "TextEditor";
             StartPosition = FormStartPosition.CenterScreen;
@@ -111,8 +133,8 @@
 
         #endregion
 
-        private OpenFileDialog openFileDialog1;
-        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog OpenFileDialog;
+        private SaveFileDialog SaveFileDialog;
         private MenuStrip ActionsMenu;
         private ToolStripMenuItem открытьToolStripMenuItem;
         private ToolStripMenuItem OpenFileButton;
@@ -120,7 +142,8 @@
         private ToolStripMenuItem SaveFileButton;
         private ToolStripMenuItem SaveAsButton;
         private ToolStripMenuItem AppearanceButton;
-        private FontDialog fontDialog1;
-        private ColorDialog colorDialog1;
+        private FontDialog FontDialog;
+        private ColorDialog ColorDialog;
+        private RichTextBox TextArea;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace Task4_TextEditor
 {
     public partial class TextEditor : Form
@@ -5,6 +7,32 @@ namespace Task4_TextEditor
         public TextEditor()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TextArea.Text))
+            {
+                DialogResult result = MessageBox.Show("Вы уверены, что хотите открыть новый файл? Изменения в текущем файле не сохранятся", "Открытие файла", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (result == DialogResult.No || result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Выберите файл для чтения и редактирования";
+                ofd.InitialDirectory = "c:\\"; //начальная директория для обзора файлов
+                ofd.Filter = "Все файлы (*.*)|*.*|Файлы *.txt|*.txt |Файлы *.doc|*.doc|Файлы *.docx|*.docx";
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string FilePath = ofd.FileName;
+                    string FileText = System.IO.File.ReadAllText(FilePath);
+                    TextArea.Text = FileText;
+                }
+            }
         }
     }
 }
